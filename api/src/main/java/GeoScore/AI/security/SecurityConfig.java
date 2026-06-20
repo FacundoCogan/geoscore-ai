e28cc46a00fd211e210491021ba6d60922c557e1
Facundo Cogan <facundocogan@gmail.com>
2026-06-20 17:57:02 -0300

    delete ngrok config

--- api/src/main/java/GeoScore/AI/security/SecurityConfig.java
+++ api/src/main/java/GeoScore/AI/security/SecurityConfig.java
@@ -72,7 +72,7 @@ public class SecurityConfig {
         CorsConfiguration configuration = new CorsConfiguration();
         configuration.setAllowedOrigins(List.of(
                 "http://localhost:3000",
-                "https://geoscore-ai-pu66-a9wccqs14-facundocogans-projects.vercel.app"
+                "https://geoscore-ai-ybxx.vercel.app"
         ));
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("Authorization", "Content-Type"));
